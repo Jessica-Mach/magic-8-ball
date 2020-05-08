d196dfbd534309501a47c0d3aec537aad5d6e1e1
jessica_mach <Jessica.Mach@outlook.com>
2020-05-08 16:56:12 -0700

    Added JQuery effects

--- script.js
+++ script.js
@@ -3,27 +3,46 @@ $(document).ready(function(){
 	var magic8Ball = {};
 	magic8Ball.answers = ["Are you kidding me?", 
 		"I don't even want to dignify that with a response.", 
-		"Anything can happen if you eat a lot of garlic and wish hard enough.", 
+		"Anything can happen if you eat a lot of garlic.", 
 		"I thought you were supposed to be the smart one.",
 		"Ooh, let's change the subject", 
 		"Your mom says yes, and she is thrilled.",
 		"How can you ask me that?",
-		"Yes, of course, my answer is yes! You've made me the happiest Magic 8-Ball on the planet.",
-		"My cat's aura is emitting strong yes vibes. (Also, my cat doesn't like you.)"];
+		"Yes, of course, my answer is yes!",
+		"My cat's aura is emitting strong yes vibes.", 
+		"Not likely",
+		"The wind is changing.",
+		"Probably",
+		"I think not."];
 	
 	// Get a random answer from Magic 8 Ball	
 	magic8Ball.giveAnswer = function () {
 		var answerText = "Oops. Something is not working quite right.";
+		
 		answerText = this.answers[(Math.floor(Math.random() * this.answers.length))];
-		$("#answer").text( answerText);
+		$("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+		
+		var showAnswer = function() {
+			$("#answer").text( answerText);
+			$("#answer").fadeIn(4000);
+		};
+		
+		setTimeout(showAnswer, 500);	
 	};
 
 	// Prompt user to enter a question
 	var askQuestion = function() {
-		var question = prompt("Ask a Yes/No question!");
-		magic8Ball.giveAnswer();
+		$("#answer").hide();
+		$("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/8side.png");
+		$("#8ball").effect("shake");
+		var questionPrompt = function(){
+			var question = prompt("Ask a Yes/No question!");
+			magic8Ball.giveAnswer();
+		};
+		setTimeout(questionPrompt, 500);
 	};
 
+	$("#answer").hide();
 	$("#questionButton").click(askQuestion);
  
 });
